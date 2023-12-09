9403890cf257ca00187e8376d0d535b3b4e234f2
Ramon de Vaan <ramon.de.vaan@outlook.com>
2023-12-09 15:15:54 +0100

    Prepare Day10

---
+++ app/src/main/java/nl/ramondevaan/aoc2023/day10/Day10.java
@@ -0,0 +1,17 @@
+package nl.ramondevaan.aoc2023.day10;
+
+import java.util.List;
+
+public class Day10 {
+
+    public Day10(final List<String> lines) {
+    }
+
+    public long solve1() {
+        return 0L;
+    }
+
+    public long solve2() {
+        return 0L;
+    }
+}
