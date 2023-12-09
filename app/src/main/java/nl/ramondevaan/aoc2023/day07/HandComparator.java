a7353929d1aa8f176d5baaf216e379fcdf43eac8
Ramon de Vaan <ramon.de.vaan@outlook.com>
2023-12-09 01:18:55 +0100

    Improve day07 performance

--- app/src/main/java/nl/ramondevaan/aoc2023/day07/HandComparator.java
+++ app/src/main/java/nl/ramondevaan/aoc2023/day07/HandComparator.java
@@ -1,14 +1,40 @@
 package nl.ramondevaan.aoc2023.day07;
 
-import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
 import java.util.Comparator;
 
 import static nl.ramondevaan.aoc2023.day07.HandType.*;
 
-@RequiredArgsConstructor
 public class HandComparator implements Comparator<Hand> {
+
+    private static final int CARD_TYPES = Card.values().length;
+    private static final HandType[][][][][][] ARITY_COUNT_TO_HAND_TYPE = new HandType[6][6][6][6][6][6];
+
+    static {
+        ARITY_COUNT_TO_HAND_TYPE[0][5][0][0][0][0] = HIGH_CARD;
+        ARITY_COUNT_TO_HAND_TYPE[0][3][1][0][0][0] = ONE_PAIR;
+        ARITY_COUNT_TO_HAND_TYPE[0][1][2][0][0][0] = TWO_PAIR;
+        ARITY_COUNT_TO_HAND_TYPE[0][2][0][1][0][0] = THREE_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[0][0][1][1][0][0] = FULL_HOUSE;
+        ARITY_COUNT_TO_HAND_TYPE[0][1][0][0][1][0] = FOUR_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[0][0][0][0][0][1] = FIVE_OF_A_KIND;
+
+        ARITY_COUNT_TO_HAND_TYPE[1][4][0][0][0][0] = ONE_PAIR;
+        ARITY_COUNT_TO_HAND_TYPE[1][2][1][0][0][0] = THREE_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[1][0][2][0][0][0] = FULL_HOUSE;
+        ARITY_COUNT_TO_HAND_TYPE[1][1][0][1][0][0] = FOUR_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[1][0][0][0][1][0] = FIVE_OF_A_KIND;
+
+        ARITY_COUNT_TO_HAND_TYPE[2][3][0][0][0][0] = THREE_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[2][1][1][0][0][0] = FOUR_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[2][0][0][1][0][0] = FIVE_OF_A_KIND;
+
+        ARITY_COUNT_TO_HAND_TYPE[3][2][0][0][0][0] = FOUR_OF_A_KIND;
+        ARITY_COUNT_TO_HAND_TYPE[3][0][1][0][0][0] = FIVE_OF_A_KIND;
+
+        ARITY_COUNT_TO_HAND_TYPE[4][1][0][0][0][0] = FIVE_OF_A_KIND;
+
+        ARITY_COUNT_TO_HAND_TYPE[5][0][0][0][0][0] = FIVE_OF_A_KIND;
+    }
 
     @Override
     public int compare(final Hand hand1, final Hand hand2) {
@@ -21,8 +47,11 @@ public class HandComparator implements Comparator<Hand> {
             return comparison;
         }
 
+        final var c1 = hand1.getCards();
+        final var c2 = hand2.getCards();
+
         for (int i = 0; i < hand1.getCards().size(); i++) {
-            comparison = hand1.getCards().get(i).compareTo(hand2.getCards().get(i));
+            comparison = c1.get(i).compareTo(c2.get(i));
 
             if (comparison != 0) {
                 return comparison;
@@ -31,55 +60,19 @@ public class HandComparator implements Comparator<Hand> {
 
         return 0;
     }
+
     private HandType parse(final Hand hand) {
-        final var handType = parseWithoutJoker(hand);
-        final var numberOfJokers = Math.toIntExact(hand.getCards().stream().filter(card -> card == Card.JOKER).count());
-
-        return switch (numberOfJokers) {
-            case 0 -> handType;
-            case 1 -> switch (handType) {
-                case FOUR_OF_A_KIND -> FIVE_OF_A_KIND;
-                case THREE_OF_A_KIND -> FOUR_OF_A_KIND;
-                case TWO_PAIR -> FULL_HOUSE;
-                case ONE_PAIR -> THREE_OF_A_KIND;
-                default -> ONE_PAIR;
-            };
-            case 2 -> switch (handType) {
-                case THREE_OF_A_KIND -> FIVE_OF_A_KIND;
-                case ONE_PAIR -> FOUR_OF_A_KIND;
-                default -> THREE_OF_A_KIND;
-            };
-            case 3 -> handType == HandType.ONE_PAIR ? FIVE_OF_A_KIND : FOUR_OF_A_KIND;
-            default -> FIVE_OF_A_KIND;
-        };
-    }
-
-    private HandType parseWithoutJoker(final Hand hand) {
-        final var cardOccurrence = new int[Card.values().length];
+        final var cardOccurrence = new int[CARD_TYPES];
         for (final var card : hand.getCards()) {
-            if (card != Card.JOKER) {
-                cardOccurrence[card.ordinal()]++;
-            }
+            cardOccurrence[card.ordinal()]++;
         }
 
-        final var occurrenceValues = new int[6];
-        Arrays.stream(cardOccurrence).forEach(i -> occurrenceValues[i]++);
-
-        if (occurrenceValues[5] >= 1) {
-            return HandType.FIVE_OF_A_KIND;
-        } else if (occurrenceValues[4] >= 1) {
-            return HandType.FOUR_OF_A_KIND;
-        } else if (occurrenceValues[3] >= 1) {
-            if (occurrenceValues[2] >= 1) {
-                return HandType.FULL_HOUSE;
-            }
-            return HandType.THREE_OF_A_KIND;
-        } else if (occurrenceValues[2] == 2) {
-            return HandType.TWO_PAIR;
-        } else if (occurrenceValues[2] >= 1) {
-            return HandType.ONE_PAIR;
+        final var ac = new int[6];
+        for (int i = 1; i < cardOccurrence.length; i++) {
+            ac[cardOccurrence[i]]++;
         }
+        ac[0] = cardOccurrence[0]; // Set joker
 
-        return HandType.HIGH_CARD;
+        return ARITY_COUNT_TO_HAND_TYPE[ac[0]][ac[1]][ac[2]][ac[3]][ac[4]][ac[5]];
     }
 }
