e72409dccf2d5ed84bbd886528ae53ca68020932
Ramon de Vaan <ramon.de.vaan@outlook.com>
2023-12-11 16:41:52 +0100

    Improve performance Day11

--- app/src/main/java/nl/ramondevaan/aoc2023/day11/Day11.java
+++ app/src/main/java/nl/ramondevaan/aoc2023/day11/Day11.java
@@ -1,7 +1,5 @@
 package nl.ramondevaan.aoc2023.day11;
 
-import nl.ramondevaan.aoc2023.util.Coordinate;
-
 import java.util.List;
 
 public class Day11 {
@@ -22,33 +20,50 @@ public class Day11 {
   }
 
   private long solve(final long multiplier) {
+    final var galaxyRow = new long[galaxyImage.getRows()][galaxyImage.getColumns()];
+    final var galaxyColumn = new long[galaxyImage.getRows()][galaxyImage.getColumns()];
+
+    for (final var galaxy : galaxyImage.getGalaxies()) {
+      galaxyRow[galaxy.row()][galaxy.column()] = galaxy.row();
+      galaxyColumn[galaxy.row()][galaxy.column()] = galaxy.column();
+    }
+
+    for (final var emptyRow : galaxyImage.getEmptyRows()) {
+      for (int row = emptyRow + 1; row < galaxyImage.getRows(); row++) {
+        for (int column = 0; column < galaxyImage.getColumns(); column++) {
+          galaxyRow[row][column] += multiplier;
+        }
+      }
+    }
+
+    for (final var emptyColumn : galaxyImage.getEmptyColumns()) {
+      for (int column = emptyColumn + 1; column < galaxyImage.getColumns(); column++) {
+        for (int row = 0; row < galaxyImage.getRows(); row++) {
+          galaxyColumn[row][column] += multiplier;
+        }
+      }
+    }
+
+      return getDistanceSum(galaxyRow, galaxyColumn);
+  }
+
+
+  private long getDistanceSum(final long[][] galaxyRow, final long[][] galaxyColumn) {
     final var galaxies = galaxyImage.getGalaxies();
 
     long distanceSum = 0L;
 
     for (int i = 0; i < galaxies.size(); i++) {
+      final var from = galaxies.get(i);
+      final var fromRow = galaxyRow[from.row()][from.column()];
+      final var fromColumn = galaxyColumn[from.row()][from.column()];
       for (int j = i + 1; j < galaxies.size(); j++) {
-        distanceSum += distance(galaxies.get(i), galaxies.get(j), multiplier);
+        final var to = galaxies.get(j);
+        final var toRow = galaxyRow[to.row()][to.column()];
+        final var toColumn = galaxyColumn[to.row()][to.column()];
+        distanceSum += Math.abs(toRow - fromRow) + Math.abs(toColumn - fromColumn);
       }
     }
-
     return distanceSum;
-  }
-
-  private long distance(final Coordinate a, final Coordinate b, final long multiplier) {
-    final int minRow = Math.min(a.row(), b.row());
-    final int maxRow = Math.max(a.row(), b.row());
-    final int minColumn = Math.min(a.column(), b.column());
-    final int maxColumn = Math.max(a.column(), b.column());
-
-    final var emptyRowsBetween = galaxyImage.getEmptyRows().stream()
-            .filter(i -> minRow < i && i < maxRow).count();
-    final var emptyColumnsBetween = galaxyImage.getEmptyColumns().stream()
-            .filter(i -> minColumn < i && i < maxColumn).count();
-
-    final var rowDistance = maxRow - minRow + emptyRowsBetween * multiplier;
-    final var columnDistance = maxColumn - minColumn + emptyColumnsBetween * multiplier;
-
-    return rowDistance + columnDistance;
   }
 }
