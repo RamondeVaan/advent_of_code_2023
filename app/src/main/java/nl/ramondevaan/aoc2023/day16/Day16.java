5d0cc5d4bf5961b1f5ff5e2d89b12f67cca75c07
Ramon de Vaan <ramon.de.vaan@outlook.com>
2023-12-15 12:20:50 +0100

    Prepare Day16

---
+++ app/src/main/java/nl/ramondevaan/aoc2023/day16/Day16.java
@@ -0,0 +1,18 @@
+package nl.ramondevaan.aoc2023.day16;
+
+import java.util.List;
+
+public class Day16 {
+
+
+  public Day16(final List<String> lines) {
+  }
+
+  public long solve1() {
+    return 0L;
+  }
+
+  public long solve2() {
+    return 0L;
+  }
+}
