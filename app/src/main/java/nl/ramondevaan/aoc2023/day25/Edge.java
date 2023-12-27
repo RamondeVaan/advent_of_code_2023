3b6e27e3b80c7c14403041926ccf2bc31124e8c4
Ramon de Vaan <ramon.de.vaan@outlook.com>
2023-12-27 16:19:06 +0100

    Cleanup Day25

--- app/src/main/java/nl/ramondevaan/aoc2023/day25/Edge.java
+++
@@ -1,4 +0,0 @@
-package nl.ramondevaan.aoc2023.day25;
-
-public record Edge(String from, String to) {
-}
--- app/src/main/java/nl/ramondevaan/aoc2023/day25/Graph.java
+++
@@ -1,8 +0,0 @@
-package nl.ramondevaan.aoc2023.day25;
-
-import com.google.common.collect.ListMultimap;
-
-import java.util.List;
-
-public record Graph(List<String> vertices, ListMultimap<String, String> edges) {
-}
